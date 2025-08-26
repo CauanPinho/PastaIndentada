a5a588a3e54f55af78b7d47aa99ba7e6119766e3
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-08-26 20:05:01 -0300

    Update Exercicio03.c

--- Exercicio03.c
+++ Exercicio03.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <locale.h>
+
 
 int main()
 {
-    setlocale(LC_ALL, "portuguese");
+  
     int x,y,z;
     printf("Digite um numero");
     scanf("%d",&x);
     printf("Digite um numero");
     scanf("%d",&y);
     z=x/y;
-    printf("A divis„o entre ambos È:%d",z);
+    printf("A divis√£o entre ambos √©:%d",z);
 }
